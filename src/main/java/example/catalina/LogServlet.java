0c6011b63c8ae7665642ee88bd2314361aa2abe8
bskoglund <bjorn.skoglund@atex.com>
2011-02-05 22:57:26 +0100

    Handle missing tomcat process.

--- src/main/java/example/catalina/LogServlet.java
+++ src/main/java/example/catalina/LogServlet.java
@@ -3,6 +3,7 @@ package example.catalina;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,20 +19,28 @@ public class LogServlet extends HttpServlet {
     private static final Logger LOGGER = Logger.getLogger(LogServlet.class.getName());
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            LogFileFinder logFinder = new LogFileFinder(Runtime.getRuntime());
-            String logFile = logFinder.getLogs().get(0);
 
-            LOGGER.info("Reading logfile " + logFile);
-            InputStream is = new FileInputStream(logFile);
+            LogFileFinder logFinder = new LogFileFinder(Runtime.getRuntime());
+
+            List<String> logs = logFinder.getLogs();
+            if (logs.size() == 0) {
+                response.setStatus(503);
+                response.getOutputStream().write((new String("No tomcat process found.")).getBytes());
+                return;
+            }
+            String firstLog = logs.get(0);
+
+            LOGGER.info("Reading logfile " + firstLog);
+            InputStream is = new FileInputStream(firstLog);
             int avail = is.available();
 
             is.skip(avail - 80 * 80);
 
             byte[] data = new byte[is.available()];
             is.read(data);
-            resp.getOutputStream().write(data);
+            response.getOutputStream().write(data);
 
         } catch (ConfigException e) {
             LOGGER.log(Level.WARNING, "An error occurred.", e);
